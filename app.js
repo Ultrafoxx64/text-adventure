31a6b0599ae999b51bb03a7910855302b0ebc63d
Mel <amelia_deitz@yahoo.com>
2022-01-26 15:03:37 -0800

    q q q q quit exi :qa :qa! :qa stop $ git checkout MelDraft

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 // document.getElementById('Anything').innerHTML="Hello World";
 
-const person = {
+//Easier to make an array for the characters and leave it as const person?//
+// Is that possible to have the person and their stats listed in the array?//
+//Or do you need a seperate const for every character? There has to be a way//
+//that you can just make a general 'player' function and then further specify, no?//
+//Is this where nesting comes in?//
+const Melstats = {
     name: "Mel",
+    hp: 500,
+    atk: 7,
+    def: 6,
+};
+
+//document.getElementById("Stats").innerHTML =  Melstats;//
+//not what works, but am I on the right track, sorta?//
+//basically want it to be like onclick -> display stats of chosen character//
+
+const Pocket = {
+    name: "Pocket",
+    hp: 500,
+    atk: 7,
+    def: 6
+};
+
+const Spider = {
+    name: "Spider",
     hp: 500,
     atk: 7,
     def: 6
 };
 
 const enemy = {
-    name: "EVIL Mel",
+    name: "Apparition",
     hp: 200,
     atk: 10,
-    def: 10
+    def: 10,
+
+    //name:"Skeleton",
+    //hp: 500,
+    //atk: 2,
+    //def: 10,
 };
 
 
+console.log("Player Stats:", Melstats.name, "HP", Melstats.hp, "Atk", Melstats, "Def", Melstats.def);
 
-console.log("Player Stats:", person.name, "HP", person.hp, "Atk", person.atk, "Def", person.def);
 
-
-document.getElementById('Name').innerHTML=person.name
-document.getElementById('HP').innerHTML=person.hp
-document.getElementById('ATK').innerHTML=person.atk
-document.getElementById('DEF').innerHTML=person.def
+document.getElementById('Name').innerHTML=Melstats.name
+document.getElementById('HP').innerHTML=Melstats.hp
+document.getElementById('ATK').innerHTML=Melstats.atk
+document.getElementById('DEF').innerHTML=Melstats.def
 
 const Fight = () => {
     enemy.hp = enemy.hp-Multiplier(person.atk) + Multiplier(enemy.def); 
@@ -35,3 +62,13 @@ const Fight = () => {
 const Multiplier = (input) => {
     return Math.floor(Math.random()*input)
 }
+
+const NoHeals = () => {
+    if (enemy.hp > 200) 
+    return enemy.hp;
+}
+
+//this what I need to replace h1 with h2, maybe?//
+function reset(){
+    setTimeout(document.getElementById('h1').innerHTML='h2', 200);
+  }
